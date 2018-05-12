bd6abea07de33981cffc404014a5f77e58c3d862
Richard Session <info@richardsession.com>
2018-05-12 11:48:10 -0400

    Add license and update plugin info

--- wp-disable-password-change-notification-plugin.php
+++ wp-disable-password-change-notification-plugin.php
@@ -1,7 +1,15 @@
 <?php
 /*
-Plugin Name: Disable Password Change Notifications
-Description: Disable password update emails in WordPress for admins
+	Plugin Name: Disable Password Change Notifications
+	Plugin URI: https://github.com/richardsession/wp-disable-password-change-notification
+	Description: Disable password update emails in WordPress for admins
+	Version: 1.0.0
+	Author: Richard Session
+	Author URI: https://richardsession.com
+	Text Domain: password
+	GitHub Plugin URI: https://github.com/richardsession/wp-disable-password-change-notification
+	GitHub Branch: master
+	License: MIT
 */
 
 if ( !function_exists( 'wp_password_change_notification' ) ) {
